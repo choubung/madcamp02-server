eb49417e4cb82558916db90c8382b938864a3167
Kim Wonjung <96229091+wjhjkim@users.noreply.github.com>
2024-07-06 19:44:39 +0900

    Update server.js: repair some error

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (!mongoUri) {
   process.exit(1);
 }
 
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUri)
   .then(() => {
     console.log('MongoDB connected');
   })
